a3cfb6b87e3a99572d59e3f5955bf46783fba109
Silent Malachite <84108752+SilentMalachite@users.noreply.github.com>
2026-06-14 22:45:46 +0900

    refactor: gettext-wrap UI strings in service-user index

--- lib/ayumi_web/live/service_user_live/index.ex
+++ lib/ayumi_web/live/service_user_live/index.ex
@@ -7,7 +7,7 @@ defmodule AyumiWeb.ServiceUserLive.Index do
   def mount(_params, _session, socket) do
     {:ok,
      socket
-     |> assign(:page_title, "利用者一覧")
+     |> assign(:page_title, gettext("利用者一覧"))
      |> assign(:service_users, Plans.list_service_users())}
   end
 
@@ -16,18 +16,18 @@ defmodule AyumiWeb.ServiceUserLive.Index do
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
       <.header>
-        利用者一覧
+        {gettext("利用者一覧")}
         <:actions>
-          <.button navigate={~p"/service_users/new"}>新規登録</.button>
+          <.button navigate={~p"/service_users/new"}>{gettext("新規登録")}</.button>
         </:actions>
       </.header>
 
       <.table id="service-users" rows={@service_users}>
-        <:col :let={su} label="氏名">
+        <:col :let={su} label={gettext("氏名")}>
           <.link navigate={~p"/service_users/#{su.id}"}>{su.name}</.link>
         </:col>
-        <:col :let={su} label="ふりがな">{su.name_kana}</:col>
-        <:col :let={su} label="受給者証番号">{su.recipient_cert_number}</:col>
+        <:col :let={su} label={gettext("ふりがな")}>{su.name_kana}</:col>
+        <:col :let={su} label={gettext("受給者証番号")}>{su.recipient_cert_number}</:col>
       </.table>
     </Layouts.app>
     """
